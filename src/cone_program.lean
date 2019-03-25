20f4b5438c22fdd95e5a3d211bc497cff4716594
Alexander Bentkamp <a.bentkamp@vu.nl>
2019-03-25 14:23:25 -0400

    more

--- src/cone_program.lean
+++ src/cone_program.lean
@@ -2,6 +2,8 @@ import .inner_product_space .cone data.real.basic .matrix
 
 open matrix
 
+noncomputable theory
+
 variables {k m n : nat}
 
 namespace cone_program
@@ -11,38 +13,56 @@ variables {α : Type} [add_comm_group α] [real_inner_product_space α] (K : set
 structure primal (α : Type) (m : nat) := 
 (obf : α)
 (lhs : vector α m)
-(rhs : vector real m)
+(rhs : vector ℝ m)
+
+def mat_mul_vec (A : vector α m) (x : α) : (vector ℝ m) := A.map (λ (a : α), ⟪ a, x ⟫)
+
+local infix `⊚` : 70 := mat_mul_vec
+
+def vec_mul_mat (A : vector α m) (y : vector ℝ m) : α := (vector.map₂ (•) y A).sum
+
+local infix `⊙` : 70 := vec_mul_mat
 
 def primal.feasible (P : primal α m) (x : α) : Prop := 
 let ⟨c,A,b⟩ := P in
-(∀ i : fin m, ⟪A.nth i, x⟫ = b.nth i) ∧ x ∈ K
+A ⊚ x = b ∧ x ∈ K
 
 def primal.optimal (P : primal α n) (x : α) : Prop := 
 let c := P.obf in
-primal.feasible K P x ∧ (∀ y : α, primal.feasible K P y → ⟪c, x⟫ ≤ ⟪c, y⟫)
+P.feasible K x ∧ ∀ y, P.feasible K y → ⟪c, x⟫ ≤ ⟪c, y⟫
 
 structure dual (α : Type) (m : nat) := 
 (obf : vector real m)
 (lhs : vector α m)
 (rhs : α)
 
+
 def dual.feasible (D : dual α m) (y : vector real m) : Prop := 
 let ⟨b,A,c⟩ := D in
-c - (vector.map₂ (•) y A).sum ∈ K'.
+c - A ⊙ y ∈ K'.
 
 def dual.optimal (D : dual α m) (y : vector real m) : Prop := 
 let b := D.obf in
-dual.feasible K' D y ∧ (∀ x : vector real m, dual.feasible K' D x → (b ⬝ x) ≤ (b ⬝ y))
+D.feasible K' y ∧ ∀ x, D.feasible K' x → b ⬝ x ≤ b ⬝ y
 
 def primal.to_dual : primal α m → dual α m
-| ⟨C,A,b⟩ := ⟨b,A,C⟩ 
+| ⟨c,A,b⟩ := ⟨b,A,c⟩ 
 
 lemma lower_bound {α : Type} [hα : add_comm_group α] [hα' : real_inner_product_space α] 
 (K : set α) (hK : cone K)
-(P : primal α m) (x_opt : α) (h_x_opt : primal.optimal K P x_opt) :
-  let D := primal.to_dual P in 
-  let K' := dual_cone K in 
-  ∀ (y : vector ℝ m) (hy : dual.feasible K' D y), (D.obf ⬝ y) ≤ ⟪ P.obf, x_opt ⟫ :=
-sorry
+(P : primal α m) (x : α) (h_x_opt : P.optimal K x) 
+(y : vector ℝ m) (hy : P.to_dual.feasible (dual_cone K) y) : 
+P.to_dual.obf ⬝ y ≤ ⟪ P.obf, x ⟫ :=
+begin
+  let c := P.obf,
+  let A := P.lhs,
+  let b := P.rhs,
+
+  have h : - b ⬝ y = ⟪ c, x ⟫ - ⟪ A ⊙ y + c, x ⟫ + y ⬝ (A ⊚ x - b),
+  begin
+    simp [real_inner_product_space.inner_add_left],
+  end,
+
+end
 
 end cone_program
