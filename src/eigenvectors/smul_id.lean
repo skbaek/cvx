5149f5a8e64658317d985f467d0331cff7b9dd77
Alexander Bentkamp <a.bentkamp@vu.nl>
2019-08-14 14:14:13 +0200

    more

--- src/eigenvectors/smul_id.lean
+++ src/eigenvectors/smul_id.lean
@@ -5,10 +5,13 @@ universes v w
 
 section smul_id
 variables {α : Type v} {β : Type w}
+variables [comm_ring α] [add_comm_group β] [module α β] (a : α)
 
-def smul_id [comm_ring α] [add_comm_group β] [module α β] (a : α) : β →ₗ[α] β := a • linear_map.id
+def smul_id : β →ₗ[α] β := a • linear_map.id
 
-instance smul_id.is_semiring_hom [comm_ring α] [add_comm_group β] [module α β] : 
+lemma smul_id_apply (x : β) : ((smul_id a : β →ₗ[α] β) : β → β) x = a • x := rfl
+
+instance smul_id.is_semiring_hom : 
 is_semiring_hom (smul_id : α → β →ₗ[α] β) := {
   map_zero := begin unfold smul_id, ext, simp end,
   map_one := begin unfold smul_id, ext, simp end,
@@ -16,14 +19,14 @@ is_semiring_hom (smul_id : α → β →ₗ[α] β) := {
   map_mul := begin unfold smul_id, intros, ext, simp [mul_smul] end
 }
 
-instance smul_id.is_ring_hom [comm_ring α] [add_comm_group β] [module α β] : 
+instance smul_id.is_ring_hom : 
 is_ring_hom (smul_id : α → β →ₗ[α] β) := {
   map_one := smul_id.is_semiring_hom.map_one,
   map_add := smul_id.is_semiring_hom.map_add,
   map_mul := smul_id.is_semiring_hom.map_mul
 }
 
-instance [comm_ring α] [add_comm_group β] [module α β] : algebra α (β →ₗ[α] β) := 
+instance : algebra α (β →ₗ[α] β) := 
 { to_fun := smul_id,
   commutes' := 
   begin 
