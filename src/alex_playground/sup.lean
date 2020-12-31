3afb22e77308befc16814ae0ca3c4d98afb83f89
Alexander Bentkamp <a.bentkamp@vu.nl>
2020-12-31 16:22:59 +0100

    more

--- src/alex_playground/sup.lean
+++ src/alex_playground/sup.lean
@@ -28,23 +28,11 @@ end curried
 
 def optimal_value {α : Type*} [curried α ℝ] (f : α) : ℝ := Sup (range f)
 
-def optimal_point {α : Type*} [curried α ℝ] (f : α) := {x : curried.uncurried_arg_type α ℝ // ↿f x = optimal_value f}
-
-def optimal_point' {α : Type*} [curried α ℝ] (f : α) := {x : curried.uncurried_arg_type α ℝ // ∀ y, ↿f x ≥ ↿f y}
-
-
-notation `subject_to ` binders ` maximize ` f:(scoped Q, Q) := optimal_point' f
+def optimal_point {α : Type*} [curried α ℝ] (f : α) := {x : curried.uncurried_arg_type α ℝ // ∀ y, ↿f x ≥ ↿f y}
+def optimal_point' {α : Type*} [curried α ℝ] (f : α) := {x : curried.uncurried_arg_type α ℝ // ↿f x = optimal_value f}
 
 
-
-def my_optimal_point1 : optimal_point (λ (x y : ℝ) (h : x ≤ 0) (h : y ≤ 0), x + y) := sorry
-
-def my_optimal_point2 : optimal_point (λ (x : ℝ) (h : x ≤ 0), x) :=
-⟨(psigma.mk 0 (le_refl 0) : Σ' x : ℝ, x ≤ 0), sorry⟩
-
-
-def my_optimal_point3 : subject_to (x y : ℝ) (h : x ≤ 0) (h : y ≤ 0) maximize x + y := sorry
-
+notation `subject_to ` binders ` maximize ` f:(scoped Q, Q) := optimal_point f
 
 
 def transform {α β : Type*} [curried α ℝ] [curried β ℝ] 
@@ -52,50 +40,20 @@ def transform {α β : Type*} [curried α ℝ] [curried β ℝ]
   (t : curried.uncurried_arg_type α ℝ → curried.uncurried_arg_type β ℝ)
   (t' : curried.uncurried_arg_type β ℝ → curried.uncurried_arg_type α ℝ)
   (ht : ∀ x, ↿f x = ↿g (t x)) (ht' : ∀ x, ↿f (t' x) = ↿g x)
-  (p : optimal_point' f) : optimal_point' g := 
+  (p : optimal_point f) : optimal_point g := 
 ⟨t p.val, λ y, begin rw [←ht, ←ht'], apply p.property (t' y) end⟩
-
-
-#check (psigma.mk 3 (psigma.mk 3 rfl) : Σ' (x : ℕ), Σ' (y : ℕ), (x = y))
-
-
-def my_optimal_point4 
-  (h : subject_to (y : ℝ) maximize 5 + y) : 
-  subject_to (x y : ℝ) (h : x = 5) maximize x + y :=
-begin
-  apply transform (λ (y : ℝ), 5 + y) (λ (x y : ℝ) (h : x = 5), x + y)
-    (λ p, (psigma.mk 5 (psigma.mk p rfl) : Σ' (x : ℝ), Σ' (y : ℝ), (x = 5)))
-    (@curried.uncurry (Π (x y : ℝ) (h : x = 5), ℝ) ℝ _ (λ (x y : ℝ) (h : x = 5), y))--(λ p, p.2.1)
-    (λ p, rfl) 
-    begin intro p, unfold curried.uncurry, simp, exact p.2.2.symm end,
-  exact h,
-end
 
 open tactic
 open expr
 setup_tactic_parser
 
 meta def get_obj_fun : expr → tactic expr
-| `(optimal_point' %%a) := return a
+| `(optimal_point %%a) := return a
 | _ := fail "no optimization problem"
 
 meta def get_vars : expr → list (name × expr)
 | (lam var_name bi var_type body) := (var_name, var_type) :: get_vars body
 | _ := []
-
-meta def get_uncurried_argtype (vars: list (name × expr)) : tactic expr :=
-  match vars with
-  | [] := fail "not a valid objective function"
-  | [v] := return v.2
-  | v :: vars' := 
-    do 
-      r <- get_uncurried_argtype,
-      let n := v.1,
-      let ty := v.2,
-      return `(psigma (λ x : %%ty, ℕ))
-  end
-
-#check tactic.interactive.change
 
 meta def curry_local : list (name × expr) → name → list expr → tactic unit
 | [] nm locals := fail "nothing to curry"
@@ -163,14 +121,8 @@ do
 meta def tactic.interactive.transformtac (new : parse texpr) : tactic unit :=
   transformtac new
 
-#check tactic.rcases
-#check tactic.apply
-#check tactic.interactive.apply
-#check tactic.interactive.cases
 
-
-#check all_goals
-
+-- Eliminate a fixed variable
 def my_optimal_point5 : 
   subject_to (x y : ℝ) (h : x = 5) maximize x + y :=
 begin
@@ -181,5 +133,23 @@ begin
   { exact y },
   { refl },
   { rw h },
+  sorry
+end
+
+-- Introduce a slack variable
+def my_optimal_point6 : 
+  subject_to (x y : ℝ) (h : x + y ≥ 5) maximize x / y :=
+begin
+  transformtac (subject_to (x y z : ℝ) (h : z = x + y) (hz : z ≥ 5) maximize x / y),
+  { exact x },
+  { exact y },
+  { linarith },
+  { exact x },
+  { exact y },
+  { exact x + y },
+  { refl },
+  { exact h },
+  { refl },
+  { refl },
   sorry
 end
