a8ad46f0e521b24322f453a52db23d02cd882e94
Alexander Bentkamp <a.bentkamp@vu.nl>
2020-12-31 16:14:11 +0100

    more

--- src/alex_playground/sup.lean
+++ src/alex_playground/sup.lean
@@ -83,34 +83,103 @@ meta def get_vars : expr → list (name × expr)
 | (lam var_name bi var_type body) := (var_name, var_type) :: get_vars body
 | _ := []
 
+meta def get_uncurried_argtype (vars: list (name × expr)) : tactic expr :=
+  match vars with
+  | [] := fail "not a valid objective function"
+  | [v] := return v.2
+  | v :: vars' := 
+    do 
+      r <- get_uncurried_argtype,
+      let n := v.1,
+      let ty := v.2,
+      return `(psigma (λ x : %%ty, ℕ))
+  end
+
+#check tactic.interactive.change
+
+meta def curry_local : list (name × expr) → name → list expr → tactic unit
+| [] nm locals := fail "nothing to curry"
+| [(v, t)] nm locals := do
+  l <- get_local nm,
+  change_core (t.instantiate_vars locals) (some l),
+  rename nm v
+| ((v, t) :: vars') nm locals := do
+  lnm <- get_local nm, 
+  cases_core lnm [v, nm],
+  lv <- get_local v,
+  curry_local vars' nm (lv :: locals)
+
+meta def curry_goal : list (name × expr) → list expr → tactic unit
+| [] goals := fail "nothing to curry"
+| [(v, t)] goals := do
+  change (t.instantiate_vars goals),
+  enable_tags true,
+  set_main_tag [v.to_string]
+| ((v, t) :: vars') goals :=
+  focus1 (do
+    applyc `psigma.mk,
+    swap,
+    gs <- get_goals,
+    focus' [curry_goal [(v, t)] goals, curry_goal vars' (gs.head :: goals)])
+
+meta def curry1 (vars1: list (name × expr)) (vars2: list (name × expr)) : tactic unit :=
+do
+  nm <- get_unused_name,
+  intro nm,
+  curry_local vars1 nm [],
+  curry_goal vars2 []
+
+meta def curry2 (vars: list (name × expr)) : tactic unit :=
+do 
+  nm <- get_unused_name,
+  intro nm,
+  curry_local vars nm [],
+  `[dsimp [curried.uncurry, id]],
+  skip
+
 meta def transformtac (new : pexpr) : tactic unit :=
 do
   old_obj_fun <- target >>= get_obj_fun,
   new_obj_fun <- to_expr new >>= get_obj_fun, 
   old_obj_fun_type <- infer_type old_obj_fun,
   new_obj_fun_type <- infer_type new_obj_fun,
-  trace (get_vars old_obj_fun),
-  trace (get_vars new_obj_fun),
-  refine ``(@transform 
-    %%new_obj_fun_type %%old_obj_fun_type _ _
-    %%new_obj_fun %%old_obj_fun 
-    _ --(λ x, ⟨_, _, _⟩ : ℝ → Σ' (x y : ℝ), x = 5) 
-    _ --(λ x : Σ' (x y : ℝ), x = 5, (x.cases_on (λ x yh, _) : ℝ)) 
-    _ _ _)
+  focus1 (do
+    refine ``(@transform 
+      %%new_obj_fun_type %%old_obj_fun_type _ _
+      %%new_obj_fun %%old_obj_fun 
+      _ --(λ x, ⟨_, _, _⟩ : ℝ → Σ' (x y : ℝ), x = 5) 
+      _ --(λ x : Σ' (x y : ℝ), x = 5, (x.cases_on (λ x yh, _) : ℝ)) 
+      _ _ _),
+    focus' [
+      curry1 (get_vars new_obj_fun) (get_vars old_obj_fun), 
+      curry1 (get_vars old_obj_fun) (get_vars new_obj_fun), 
+      curry2 (get_vars new_obj_fun),
+      curry2 (get_vars old_obj_fun),
+      skip])
+    -- intro `x,
+    -- to_expr ```(x) >>= cases_core,)
 
 
 meta def tactic.interactive.transformtac (new : parse texpr) : tactic unit :=
   transformtac new
 
+#check tactic.rcases
 #check tactic.apply
 #check tactic.interactive.apply
 #check tactic.interactive.cases
 
 
-def my_optimal_point4 : 
+#check all_goals
+
+def my_optimal_point5 : 
   subject_to (x y : ℝ) (h : x = 5) maximize x + y :=
 begin
   transformtac (subject_to (y : ℝ) maximize 5 + y),
-  
-  --refine @transform (ℝ → ℝ) (Π (x y : ℝ) (h : x = 5), ℝ) _ _ _ _ _ _ _ _ _,
+  { exact 5 },
+  { exact y },
+  { refl },
+  { exact y },
+  { refl },
+  { rw h },
+  sorry
 end
